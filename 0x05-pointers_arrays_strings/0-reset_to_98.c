87cd54f329850a85c4d0d26b7c0cc9224593c504
Filimon-H <filimon3570@gmail.com>
2021-06-29 11:56:46 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,19 @@
 #include "holberton.h"
+
+
+
 /**
-*reset_to_98 - takes a pointer to an int and updates its value to 98
-*@n: pointer n
-*/
+
+ * reset_to_98 - takes a pointer to an int and updates its value to 98
+
+ * @n: pointer n
+
+ */
+
 void reset_to_98(int *n)
+  
 {
-*n = 98;
+  
+  *n = 98;
+  
 }
